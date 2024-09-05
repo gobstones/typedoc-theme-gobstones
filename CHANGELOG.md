632e01b8cc96a40c4f899f71a318dcf8103191b6
Alan Rodas Bonjour <alanrodas@gmail.com>
2024-09-05 15:24:55 -0300

    fix: privateRemarks should span multiple paragraphs
    The current implementation expects that privateRemarks is followed by one and only one paragraph.
    This commit fixes this issue allowing that multiple paragraphs can be used as privateRemarks.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## <small>0.3.1 (2024-09-05)</small>
+
+-   fix: privateRemarks should span multiple paragraphs ([032c993](https://github.com/gobstones/typedoc-theme-gobstones/commit/032c993))
+-   build: updated gobstones-scripts to 0.9.2 ([42a28e0](https://github.com/gobstones/typedoc-theme-gobstones/commit/42a28e0))
+
 ## 0.3.0 (2024-09-04)
 
 ## <small>0.2.1 (2024-08-09)</small>
--- src/static/assets/js/typedoc-theme-gobstones.js
+++ src/static/assets/js/typedoc-theme-gobstones.js
@@ -4,9 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const privateRemarks = document.getElementById('Private Remarks');
     if (!privateRemarks) return;
     const privateRemarksTitle = privateRemarks.parentElement;
-    const privateRemarksText = privateRemarksTitle.nextElementSibling;
     privateRemarksTitle.classList.add('tsd-is-internal');
     privateRemarksTitle.classList.add('tsd-private-remarks');
-    privateRemarksText.classList.add('tsd-is-internal');
-    privateRemarksText.classList.add('tsd-private-remarks');
+
+    let privateRemarksText = privateRemarksTitle.nextElementSibling;
+    while (privateRemarksText) {
+        privateRemarksText.classList.add('tsd-is-internal');
+        privateRemarksText.classList.add('tsd-private-remarks');
+        privateRemarksText = privateRemarksText.nextElementSibling;
+    }
 });
